4b3819a9478050ac7426b1355e3dd3029f416e41
Theodore Cooper <ccooperr2005@gmail.com>
2025-01-24 02:07:54 +0800

    add pop windows

--- qrhelper.py
+++ qrhelper.py
@@ -1,16 +1,21 @@
 # This Python file uses the following encoding: utf-8
 import sys
 import cv2
+import re
 import zxingcpp
+import webbrowser
 import numpy as np
-from PySide6.QtWidgets import QApplication, QMainWindow, QWidget
-from PySide6.QtGui import QPixmap, QImage, QGuiApplication, QScreen, QPainter, QPen, QColor
-from PySide6.QtCore import Qt, QTimer
+import keyboard
+from PySide6.QtWidgets import QApplication, QMainWindow, QWidget, QSystemTrayIcon, QMenu, QVBoxLayout, QLabel, QPushButton
+from PySide6.QtGui import QPixmap, QImage, QGuiApplication, QScreen, QPainter, QPen, QColor, QIcon, QAction
+from PySide6.QtCore import Qt, QTimer, QMetaObject, Slot
+
 # Important:
 # You need to run the following command to generate the ui_form.py file
-#     pyside6-uic form.ui -o ui_form.py, or
-#     pyside2-uic form.ui -o ui_form.py
+#     pyside6-uic form.ui -o ui_form.py
 from ui_form import Ui_QrHelper
+from ui_codebarwindow import Ui_CodeBarWindow
+import res_rc
 
 class Overlay(QWidget):
     def __init__(self):
@@ -27,25 +32,89 @@ class Overlay(QWidget):
         painter.setPen(QPen(Qt.red, 10))
         painter.drawRect(self.rect())
 
+class CodeBarWindow(QWidget):
+    def __init__(self, text, format, content, position, close_callback, parent=None):
+        super(CodeBarWindow, self).__init__(parent)
+        self.ui = Ui_CodeBarWindow()
+        self.ui.setupUi(self)
+        self.setWindowFlags(Qt.Tool | Qt.WindowStaysOnTopHint | Qt.FramelessWindowHint)
+        self.setAttribute(Qt.WA_TranslucentBackground)
+        # self.setWindowOpacity(1)
+
+        self.close_callback = close_callback
+
+        if self.is_url(text):
+            self.ui.text_box.setText(f"<a href='{text}' style='color:#7DF9FF; text-decoration:underline; font-style:italic;'>{text}</a>")
+            self.ui.open_button.setEnabled(True)
+        else:
+            self.ui.text_box.setText(text)
+            self.ui.open_button.setEnabled(False)
+
+        self.ui.copy_button.clicked.connect(self.copy_text)
+        self.ui.open_button.clicked.connect(self.open_text)
+        self.ui.close_button.clicked.connect(self.close_window)
+
+    def copy_text(self):
+        clipboard = QApplication.clipboard()
+        clipboard.setText(self.ui.text_box.toPlainText())
+        self.close_callback(self)
+        self.close()
+
+    def open_text(self):
+        webbrowser.open(self.ui.text_box.toPlainText())
+        self.close_callback(self)
+        self.close()
+
+    def close_window(self):
+        self.close_callback(self)
+        self.close()
+
+    def is_url(self, text):
+        url_regex = re.compile(
+            r'^[a-zA-Z][a-zA-Z\d+\-.]*://'
+            r'(?:(?:[A-Z0-9](?:[A-Z0-9-]{0,61}[A-Z0-9])?\.)+(?:[A-Z]{2,6}\.?|[A-Z0-9-]{2,}\.?)|'
+            r'localhost|' # localhost
+            r'\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}|' # ipv4
+            r'\[?[A-F0-9]*:[A-F0-9:]+\]?)' # ipv6
+            r'(?::\d+)?' # port
+            r'(?:/?|[/?]\S+)$', re.IGNORECASE)
+        return re.match(url_regex, text) is not None
+
 class QrHelper(QMainWindow):
     def __init__(self, parent=None):
         super(QrHelper, self).__init__(parent)
         self.ui = Ui_QrHelper()
         self.ui.setupUi(self)
         self.overlay = Overlay()
+        self.codebar_windows = []
 
-        self.capture_and_decode()
+        self.tray_icon = QSystemTrayIcon(self)
+        self.tray_icon.setIcon(QIcon(":/icons/icon.png"))
+        tray_menu = QMenu()
+        show_action = QAction("Show", self)
+        quit_action = QAction("Quit", self)
+        tray_menu.addAction(show_action)
+        tray_menu.addAction(quit_action)
+        self.tray_icon.setContextMenu(tray_menu)
+        show_action.triggered.connect(self.show)
+        quit_action.triggered.connect(self.exit_app)
+        self.tray_icon.show()
 
+        self.setWindowIcon(QIcon(":/icons/icon.png"))
+
+        keyboard.add_hotkey('ctrl+shift+q', lambda: QMetaObject.invokeMethod(self, "capture_and_decode", Qt.QueuedConnection))
+
+    @Slot()
     def capture_and_decode(self):
         screenshot = self.capture_fullscreen()
         self.decode_qr(screenshot)
 
     def capture_fullscreen(self):
-        self.overlay.showFullScreen()
+        QMetaObject.invokeMethod(self.overlay, "showFullScreen", Qt.QueuedConnection)
 
         screen = QGuiApplication.primaryScreen()
         screenshot = screen.grabWindow(0)  # pixmap screenshot
-        QTimer.singleShot(500, lambda: self.overlay.hide())
+        QTimer.singleShot(500, lambda: QMetaObject.invokeMethod(self.overlay, "hide", Qt.QueuedConnection))
         return screenshot
 
     def decode_qr(self, screenshot):
@@ -60,7 +129,35 @@ class QrHelper(QMainWindow):
                   f'\n Format:   {barcode.format}'
                   f'\n Content:  {barcode.content_type}'
                   f'\n Position: {barcode.position}')
+            self.show_code_bar_window(barcode)
         return 0
+
+    def show_code_bar_window(self, barcode):
+        position = barcode.position
+        screen = QGuiApplication.primaryScreen().availableGeometry()
+        x = position.bottom_left.x
+        y = position.bottom_left.y
+        width = 420
+        height = 62
+
+        if x + width > screen.width():
+            x = screen.width() - width
+        if y + height > screen.height():
+            y = screen.height() - height
+        if x < 0:
+            x = 0
+        if y < 0:
+            y = 0
+
+        code_window = CodeBarWindow(barcode.text, barcode.format, barcode.content_type, barcode.position, self.remove_code_bar_window)
+        code_window.setGeometry(int(x), int(y), width, height)
+        code_window.show()
+        self.codebar_windows.append(code_window)  # Keep reference to the window
+
+    def remove_code_bar_window(self, code_window):
+        if code_window in self.codebar_windows:
+            self.codebar_windows.remove(code_window)
+        code_window.deleteLater()
 
     def pixmap2cv(self, pixmap):
         qimage = pixmap.toImage()
@@ -71,12 +168,16 @@ class QrHelper(QMainWindow):
         return cv2.cvtColor(arr, cv2.COLOR_RGBA2BGR)
 
     def closeEvent(self, event):
-        self.overlay.close()
-        event.accept()
+        self.hide()
+        event.ignore()
 
+    def exit_app(self):
+        self.tray_icon.hide()
+        QApplication.instance().quit()
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
+    app.setQuitOnLastWindowClosed(False)
     widget = QrHelper()
-    widget.show()
+    widget.hide()
     sys.exit(app.exec())
---
+++ ui_codebarwindow.py
@@ -0,0 +1,70 @@
+# -*- coding: utf-8 -*-
+
+################################################################################
+## Form generated from reading UI file 'codebarwindow.ui'
+##
+## Created by: Qt User Interface Compiler version 6.8.1
+##
+## WARNING! All changes made in this file will be lost when recompiling UI file!
+################################################################################
+
+from PySide6.QtCore import (QCoreApplication, QDate, QDateTime, QLocale,
+    QMetaObject, QObject, QPoint, QRect,
+    QSize, QTime, QUrl, Qt)
+from PySide6.QtGui import (QBrush, QColor, QConicalGradient, QCursor,
+    QFont, QFontDatabase, QGradient, QIcon,
+    QImage, QKeySequence, QLinearGradient, QPainter,
+    QPalette, QPixmap, QRadialGradient, QTransform)
+from PySide6.QtWidgets import (QApplication, QPushButton, QSizePolicy, QTextEdit,
+    QWidget)
+import res_rc
+
+class Ui_CodeBarWindow(object):
+    def setupUi(self, CodeBarWindow):
+        if not CodeBarWindow.objectName():
+            CodeBarWindow.setObjectName(u"CodeBarWindow")
+        CodeBarWindow.resize(370, 60)
+        sizePolicy = QSizePolicy(QSizePolicy.Policy.Fixed, QSizePolicy.Policy.Fixed)
+        sizePolicy.setHorizontalStretch(0)
+        sizePolicy.setVerticalStretch(0)
+        sizePolicy.setHeightForWidth(CodeBarWindow.sizePolicy().hasHeightForWidth())
+        CodeBarWindow.setSizePolicy(sizePolicy)
+        CodeBarWindow.setMinimumSize(QSize(370, 60))
+        self.text_box = QTextEdit(CodeBarWindow)
+        self.text_box.setObjectName(u"text_box")
+        self.text_box.setGeometry(QRect(0, 0, 300, 60))
+        self.text_box.setReadOnly(True)
+        self.close_button = QPushButton(CodeBarWindow)
+        self.close_button.setObjectName(u"close_button")
+        self.close_button.setGeometry(QRect(300, 35, 70, 25))
+        icon = QIcon()
+        icon.addFile(u":/icons/close.png", QSize(), QIcon.Mode.Normal, QIcon.State.Off)
+        self.close_button.setIcon(icon)
+        self.copy_button = QPushButton(CodeBarWindow)
+        self.copy_button.setObjectName(u"copy_button")
+        self.copy_button.setGeometry(QRect(300, 0, 35, 35))
+        icon1 = QIcon()
+        icon1.addFile(u":/icons/copy.png", QSize(), QIcon.Mode.Normal, QIcon.State.Off)
+        self.copy_button.setIcon(icon1)
+        self.copy_button.setFlat(False)
+        self.open_button = QPushButton(CodeBarWindow)
+        self.open_button.setObjectName(u"open_button")
+        self.open_button.setEnabled(False)
+        self.open_button.setGeometry(QRect(335, 0, 35, 35))
+        icon2 = QIcon()
+        icon2.addFile(u":/icons/open.png", QSize(), QIcon.Mode.Normal, QIcon.State.Off)
+        self.open_button.setIcon(icon2)
+        self.open_button.setIconSize(QSize(16, 16))
+
+        self.retranslateUi(CodeBarWindow)
+
+        QMetaObject.connectSlotsByName(CodeBarWindow)
+    # setupUi
+
+    def retranslateUi(self, CodeBarWindow):
+        CodeBarWindow.setWindowTitle(QCoreApplication.translate("CodeBarWindow", u"Form", None))
+        self.close_button.setText(QCoreApplication.translate("CodeBarWindow", u"Close", None))
+        self.copy_button.setText("")
+        self.open_button.setText("")
+    # retranslateUi
+
